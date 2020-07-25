cddf2eafeb84d3c7b44b866ec7525bf3951e8ac9
Talv Bansal <talvbansal@outlook.com>
2020-07-26 00:05:12 +0100

    Add IsSearchable trait

---
+++ src/IsSearchable.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Talvbansal\TwillSite;
+
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Trait IsSearchable
+ * @package Talvbansal\TwillSite
+ *
+ * Add a new scope to a trait to help search for content in
+ * blocks and defined fields in a searchableFields array...
+ */
+trait IsSearchable
+{
+    public function scopeSearch(Builder $query, string $search) : Builder
+    {
+        $fields = $this->getSearchableFields();
+
+        foreach ($fields as $field) {
+            $query->whereRaw("LOWER({$field}) LIKE LOWER(?)", ["%{$search}%"]);
+        }
+
+        $query->orWhereHas('blocks', function ($q) use ($search) {
+            $q->whereRaw('LOWER(content) LIKE LOWER(?)', ["%{$search}%"]);
+        });
+
+        return $query;
+    }
+
+    private function getSearchableFields(): array
+    {
+        return $this->searchableFields ?? [];
+    }
+}
